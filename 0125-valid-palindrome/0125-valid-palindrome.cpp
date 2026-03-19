2653331396dfb1de62a4a210c0aebd13779abd84
rishmakollapu <rishmakollapu@gmail.com>
2026-03-19 17:11:20 +0530

    Time: 7 ms (6.23%), Space: 9.8 MB (77.34%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            // Move left pointer to next alphanumeric
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+            // Move right pointer to previous alphanumeric
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            // Compare characters (case-insensitive)
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true; 
+    }
+};
